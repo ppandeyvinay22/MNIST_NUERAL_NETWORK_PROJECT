fb7272c63727715a13ca59e83074d0691495dd6b
technebulaa <technebulaa@nebulaa.in>
2025-03-04 23:34:41 +0530

    trained the model for 30 epocs

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
-import math
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+# loading fashion daat directly from keras
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+
+# Let's split the full training set into a validation set and a (smaller) training set. We also scale the pixel intensities down to the 0-1 range and convert them to floats, by dividing by 255.
+X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test = X_test / 255.0
+
+# plt.imshow(X_train[0], cmap="binary")
+# plt.axis("off")
+# plt.show()
+
+# Here are the corresponding class names:
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+# n_rows = 4
+# n_cols = 10
+# plt.figure(figsize=(n_cols * 1.2, n_rows * 1.2))
+# for row in range(n_rows):
+#     for col in range(n_cols):
+#         index = n_cols * row + col
+#         plt.subplot(n_rows, n_cols, index + 1)
+#         plt.imshow(X_train[index], cmap="binary", interpolation="nearest")
+#         plt.axis('off')
+#         plt.title(class_names[y_train[index]], fontsize=12)
+# plt.subplots_adjust(wspace=0.2, hspace=0.5)
+# plt.show()
+
+# making the model 
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(300, activation="relu"))
+model.add(keras.layers.Dense(100, activation="relu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+
+
+
+# now compiling the model with categorical entripy loss, stoichiomentric gradient optimizer and metrices accuracy
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer="sgd",
+              metrics=["accuracy"])
+history = model.fit(X_train, y_train, epochs=30,
+                    validation_data=(X_valid, y_valid))
